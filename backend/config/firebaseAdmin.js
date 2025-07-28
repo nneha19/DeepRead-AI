2bca906e7fe30bd167a0e2895ed576f79b712517
nneha19 <nehaghariyal19@gmail.com>
2025-07-28 22:13:17 +0530

    Refactor Firebase initialization to use environment variable for service account and update Vite config to load environment variables dynamically

--- backend/config/firebaseAdmin.js
+++ backend/config/firebaseAdmin.js
@@ -1,11 +1,15 @@
 import admin from "firebase-admin";
+import { Buffer } from 'buffer'; 
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-const firebaseServiceAccount = require("../firebaseServiceAccount.json");
+const serviceAccount = JSON.parse(
+  Buffer.from(process.env.FIREBASE_SERVICE_ACCOUNT_BASE64, 'base64').toString()
+);
+
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(firebaseServiceAccount),
+    credential: admin.credential.cert(serviceAccount),
   });
 }
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,15 +3,23 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      '/api': {
-        target: env.VITE_BACKEND_URL, // replace with your backend port
-        changeOrigin: true,
-        secure: false,
+export default defineConfig(({ mode }) => {
+
+  const env = loadEnv(mode, process.cwd(), 'VITE_');
+  
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL,
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+    define: {
+      'process.env': env
+    }
+  };
 });
